6d078ce25bfe978f00b33294027461d3cc9d70db
sunnyhhhgg <110941499+sunnyhhhgg@users.noreply.github.com>
2026-06-25 10:07:56 +0800

    Add lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/auth_provider.dart';
+import 'providers/product_provider.dart';
+import 'providers/order_provider.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const SupmartShopApp());
+}
+
+class SupmartShopApp extends StatelessWidget {
+  const SupmartShopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+        ChangeNotifierProvider(create: (_) => OrderProvider()),
+      ],
+      child: MaterialApp(
+        title: 'SUPMART',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: const Color(0xFF0A0A0A),
+          colorSchemeSeed: const Color(0xFF5E6AD2),
+          useMaterial3: true,
+          fontFamily: null,
+        ),
+        home: const SplashScreen(),
+      ),
+    );
+  }
+}
